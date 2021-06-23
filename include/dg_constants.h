9fc9aa6522153ec2482549fd7f1adf143513a20f
Toby Flynn <toby_flynn@btinternet.com>
2021-06-23 12:21:35 +0100

    Put all extern statements into single header

--- include/dg_constants.h
+++ include/dg_constants.h
@@ -1,46 +1,7 @@
 #ifndef __DG_CONSTANTS_H
 #define __DG_CONSTANTS_H
 
-extern double cubDr_g[46*15];
-extern double cubDs_g[46*15];
-extern double cubV_g[46*15];
-extern double cubVDr_g[46*15];
-extern double cubVDs_g[46*15];
-extern double cubW_g[46];
-
-extern double Dr_g[15*15];
-extern double Drw_g[15*15];
-extern double Ds_g[15*15];
-extern double Dsw_g[15*15];
-
-extern double gaussW_g[7];
-extern double gF0Dr_g[7*15];
-extern double gF0DrR_g[7*15];
-extern double gF0Ds_g[7*15];
-extern double gF0DsR_g[7*15];
-extern double gF1Dr_g[7*15];
-extern double gF1DrR_g[7*15];
-extern double gF1Ds_g[7*15];
-extern double gF1DsR_g[7*15];
-extern double gF2Dr_g[7*15];
-extern double gF2DrR_g[7*15];
-extern double gF2Ds_g[7*15];
-extern double gF2DsR_g[7*15];
-extern double gFInterp0_g[7*15];
-extern double gFInterp0R_g[7*15];
-extern double gFInterp1_g[7*15];
-extern double gFInterp1R_g[7*15];
-extern double gFInterp2_g[7*15];
-extern double gFInterp2R_g[7*15];
-extern double gInterp_g[21*15];
-extern double invMassGaussInterpT_g[15*21];
-
-extern double invMass_g[15*15];
-extern double lift_g[15*15];
-extern double mass_g[15*15];
-extern double r_g[15];
-extern double s_g[15];
-extern double ones_g[15];
+#include "dg_global_constants.h"
 
 #ifdef OP2_DG_CUDA
 #include "cublas_v2.h"
---
+++ include/dg_global_constants.h
@@ -0,0 +1,61 @@
+#ifndef __DG_GLOBAL_CONSTANTS_H
+#define __DG_GLOBAL_CONSTANTS_H
+
+extern double gam;
+extern double mu;
+extern double nu0;
+extern double nu1;
+extern double rho0;
+extern double rho1;
+extern double bc_mach;
+extern double bc_alpha;
+extern double bc_p;
+extern double bc_u;
+extern double bc_v;
+extern int FMASK[15];
+extern double ic_u;
+extern double ic_v;
+
+extern double cubV_g[46 * 15];
+extern double cubW_g[46];
+extern double cubDr_g[46*15];
+extern double cubDs_g[46*15];
+extern double cubVDr_g[46 * 15];
+extern double cubVDs_g[46 * 15];
+
+extern double gaussW_g[7];
+extern double gFInterp0_g[7 * 15];
+extern double gFInterp1_g[7 * 15];
+extern double gFInterp2_g[7 * 15];
+extern double gF0Dr_g[7 * 15];
+extern double gF0Ds_g[7 * 15];
+extern double gF1Dr_g[7 * 15];
+extern double gF1Ds_g[7 * 15];
+extern double gF2Dr_g[7 * 15];
+extern double gF2Ds_g[7 * 15];
+extern double gFInterp0R_g[7 * 15];
+extern double gFInterp1R_g[7 * 15];
+extern double gFInterp2R_g[7 * 15];
+extern double gF0DrR_g[7 * 15];
+extern double gF0DsR_g[7 * 15];
+extern double gF1DrR_g[7 * 15];
+extern double gF1DsR_g[7 * 15];
+extern double gF2DrR_g[7 * 15];
+extern double gF2DsR_g[7 * 15];
+extern double gInterp_g[21*15];
+extern double invMassGaussInterpT_g[15*21];
+
+extern double lift_drag_vec[5];
+
+extern double Dr_g[15*15];
+extern double Drw_g[15*15];
+extern double Ds_g[15*15];
+extern double Dsw_g[15*15];
+extern double invMass_g[15*15];
+extern double lift_g[15*15];
+extern double mass_g[15*15];
+extern double r_g[15];
+extern double s_g[15];
+extern double ones_g[15];
+
+#endif
