b0b82e227f231a64e3c03a04d57a8c2c3cd9ed78
Toby Flynn <toby_flynn@btinternet.com>
2021-12-04 13:40:45 +0000

    Fixed interp to max order

--- src/cuda/interp_dat_to_max_order_kernel.cu
+++ src/cuda/interp_dat_to_max_order_kernel.cu
@@ -6,12 +6,16 @@
 __device__ void interp_dat_to_max_order_gpu( const double *mats, const int *order,
                                     const double *in, double *out) {
 
-  if(*order == DG_ORDER)
-    return;
-
   const int dg_np_old = DG_CONSTANTS_cuda[(*order - 1) * 5];
   const int dg_np_new = DG_CONSTANTS_cuda[(DG_ORDER - 1) * 5];
   const double *mat = &mats[((*order - 1) * DG_ORDER + (DG_ORDER - 1)) * DG_NP * DG_NP];
+
+  if(*order == DG_ORDER) {
+    for(int i = 0; i < dg_np_new; i++) {
+      out[i] = in[i];
+    }
+    return;
+  }
 
   for(int i = 0; i < dg_np_new; i++) {
     out[i] = 0.0;
--- src/kernels/interp_dat_to_max_order.h
+++ src/kernels/interp_dat_to_max_order.h
@@ -1,12 +1,16 @@
 inline void interp_dat_to_max_order(const double *mats, const int *order,
                                     const double *in, double *out) {
   // Get constants
-  if(*order == DG_ORDER)
-    return;
-
   const int dg_np_old = DG_CONSTANTS[(*order - 1) * 5];
   const int dg_np_new = DG_CONSTANTS[(DG_ORDER - 1) * 5];
   const double *mat = &mats[((*order - 1) * DG_ORDER + (DG_ORDER - 1)) * DG_NP * DG_NP];
+
+  if(*order == DG_ORDER) {
+    for(int i = 0; i < dg_np_new; i++) {
+      out[i] = in[i];
+    }
+    return;
+  }
 
   for(int i = 0; i < dg_np_new; i++) {
     out[i] = 0.0;
