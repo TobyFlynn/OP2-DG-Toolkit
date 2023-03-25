65af756a3ce04eae8b6092b2b7a2df86873eca50
Toby Flynn <toby_flynn@btinternet.com>
2023-03-25 23:00:09 +0000

    Min in kernel memory usage

--- src/kernels/div_3d.h
+++ src/kernels/div_3d.h
@@ -9,14 +9,22 @@ inline void div_3d(const int *p, const DG_FP *dr, const DG_FP *ds,
   const DG_FP *dt_mat = &dt[(*p - 1) * DG_NP * DG_NP];
   const int dg_np = DG_CONSTANTS_TK[(*p - 1) * DG_NUM_CONSTANTS];
 
-  DG_FP tmp_r[DG_NP], tmp_s[DG_NP], tmp_t[DG_NP];
+  DG_FP tmp[DG_NP];
   for(int n = 0; n < dg_np; n++) {
-    tmp_r[n] = *rx * u[n] + *ry * v[n] + *rz * w[n];
-    tmp_s[n] = *sx * u[n] + *sy * v[n] + *sz * w[n];
-    tmp_t[n] = *tx * u[n] + *ty * v[n] + *tz * w[n];
+    tmp[n] = *rx * u[n] + *ry * v[n] + *rz * w[n];
   }
 
-  op2_in_kernel_gemv(false, dg_np, dg_np, 1.0, dr_mat, dg_np, tmp_r, 0.0, out);
-  op2_in_kernel_gemv(false, dg_np, dg_np, 1.0, ds_mat, dg_np, tmp_s, 1.0, out);
-  op2_in_kernel_gemv(false, dg_np, dg_np, 1.0, dt_mat, dg_np, tmp_t, 1.0, out);
+  op2_in_kernel_gemv(false, dg_np, dg_np, 1.0, dr_mat, dg_np, tmp, 0.0, out);
+
+  for(int n = 0; n < dg_np; n++) {
+    tmp[n] = *sx * u[n] + *sy * v[n] + *sz * w[n];
+  }
+
+  op2_in_kernel_gemv(false, dg_np, dg_np, 1.0, ds_mat, dg_np, tmp, 1.0, out);
+
+  for(int n = 0; n < dg_np; n++) {
+    tmp[n] = *tx * u[n] + *ty * v[n] + *tz * w[n];
+  }
+
+  op2_in_kernel_gemv(false, dg_np, dg_np, 1.0, dt_mat, dg_np, tmp, 1.0, out);
 }
