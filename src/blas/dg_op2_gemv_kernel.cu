2703219eda37c004093a111adb0abcb5bc144b16
Toby Flynn <toby_flynn@btinternet.com>
2023-04-24 18:56:35 +0100

    Improving GPU OP2 BLAS

--- src/blas/dg_op2_gemv_kernel.cu
+++ src/blas/dg_op2_gemv_kernel.cu
@@ -4,63 +4,81 @@
 
 #include "dg_compiler_defs.h"
 
-__device__ void _gemv_gpu(const int node, const int m, const int n,
-                          const double alpha, const double beta,
-                          const double *matrix, const double *x, double *y) {
+// CUDA kernel function
+__global__ void cuda_gemm_gpu(
+  const int m, const int n, const int incX, const int incY,
+  const double alpha, const double beta, const double * __restrict__ matrix,
+  const double * __restrict__ arg4, double * __restrict__ arg5,
+  int set_size) {
+
+  extern __shared__ double sh_mem[];
+  double *mat_sh = sh_mem;
+  double *x_sh = &sh_mem[m * n];
+
+  for(int i = threadIdx.x; i < m * n; i += blockDim.x) {
+    mat_sh[i] = matrix[i];
+  }
+
+  const int n_ = threadIdx.x + blockIdx.x * blockDim.x;
+  const int start_n = n_ - threadIdx.x;
+  const int num_cells = min((start_n + blockDim.x) / m, set_size) - (start_n / m) + 1;
+  const int start_ind = (start_n / m) * incX;
+  const int num_load = num_cells * incX;
+  for(int i = threadIdx.x; i < num_load; i += blockDim.x) {
+    x_sh[i] = arg4[start_ind + i];
+  }
+  const int local_cell_id = (n_ / m) - (start_n / m);
+  __syncthreads();
+
+  if(n_ >= set_size * m) return;
+  const int node = n_ % m;
+  const int cell = n_ / m;
+  const double *x = x_sh + local_cell_id * incX;
+  double *y = arg5 + cell * incY;
   DG_FP tmp = 0.0;
   for(int j = 0; j < n; j++) {
     int ind = DG_MAT_IND(node, j, m, n);
-    tmp += matrix[ind] * x[j];
+    tmp += mat_sh[ind] * x[j];
   }
   y[node] = beta * y[node] + alpha * tmp;
 }
 
-__device__ void _gemv_T_gpu(const int node, const int m, const int n,
-                            const double alpha, const double beta,
-                            const double *matrix, const double *x, double *y) {
+__global__ void cuda_gemm_T_gpu(
+  const int m, const int n, const int incX, const int incY,
+  const double alpha, const double beta, const double *matrix,
+  const double *__restrict arg4, double *arg5,
+  int set_size) {
+
+  extern __shared__ double sh_mem[];
+  double *mat_sh = sh_mem;
+  double *x_sh = &sh_mem[m * n];
+
+  for(int i = threadIdx.x; i < m * n; i += blockDim.x) {
+    mat_sh[i] = matrix[i];
+  }
+
+  const int n_ = threadIdx.x + blockIdx.x * blockDim.x;
+  const int start_n = n_ - threadIdx.x;
+  const int num_cells = min((start_n + blockDim.x) / n, set_size) - (start_n / n) + 1;
+  const int start_ind = (start_n / n) * incX;
+  const int num_load = num_cells * incX;
+  for(int i = threadIdx.x; i < num_load; i += blockDim.x) {
+    x_sh[i] = arg4[start_ind + i];
+  }
+  const int local_cell_id = (n_ / n) - (start_n / n);
+  __syncthreads();
+
+  if(n_ >= set_size * n) return;
+  const int node = n_ % n;
+  const int cell = n_ / n;
+  const double *x = x_sh + local_cell_id * incX;
+  double *y = arg5 + cell * incY;
   DG_FP tmp = 0.0;
   for(int j = 0; j < m; j++) {
     int ind = DG_MAT_IND(j, node, m, n);
-    tmp += matrix[ind] * x[j];
+    tmp += mat_sh[ind] * x[j];
   }
   y[node] = beta * y[node] + alpha * tmp;
-}
-
-// CUDA kernel function
-__global__ void _op_cuda_gemv(
-  const int m, const int n, const int incX, const int incY,
-  const double alpha, const double beta, const double *matrix,
-  const double *__restrict arg4, double *arg5,
-  int set_size) {
-
-
-  //process set elements
-  for(int n_ = threadIdx.x + blockIdx.x * blockDim.x;
-      n_ < set_size * m; n_ += blockDim.x * gridDim.x){
-
-    const int node = n_ % m;
-    const int cell = n_ / m;
-    _gemv_gpu(node, m, n, alpha, beta, matrix, arg4 + cell * incX,
-              arg5 + cell * incY);
-  }
-}
-
-__global__ void _op_cuda_gemv_T(
-  const int m, const int n, const int incX, const int incY,
-  const double alpha, const double beta, const double *matrix,
-  const double *__restrict arg4, double *arg5,
-  int set_size) {
-
-
-  //process set elements
-  for(int n_ = threadIdx.x + blockIdx.x * blockDim.x;
-      n_ < set_size * m; n_ += blockDim.x * gridDim.x){
-
-    const int node = n_ % n;
-    const int cell = n_ / n;
-    _gemv_T_gpu(node, m, n, alpha, beta, matrix, arg4 + cell * incX,
-              arg5 + cell * incY);
-  }
 }
 
 
@@ -82,14 +100,16 @@ void custom_kernel_gemv(op_set set, const bool t, const int m, const int n, cons
 
     //set CUDA execution parameters
     int nthread = 256;
-    int nblocks = 200;
+    const int nblocks = t ? (set->size * n / nthread + 1) : (set->size * m / nthread + 1);
 
     if(t) {
-      _op_cuda_gemv_T<<<nblocks,nthread>>>(m, n, x->dim, y->dim, alpha, beta,
+      const int ncells = nthread / n + 2;
+      cuda_gemm_T_gpu<<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m, n, x->dim, y->dim, alpha, beta,
                                            matrix_d, (double *) args[0].data_d,
                                            (double *) args[1].data_d, set->size);
     } else {
-      _op_cuda_gemv<<<nblocks,nthread>>>(m, n, x->dim, y->dim, alpha, beta,
+      const int ncells = nthread / m + 2;
+      cuda_gemm_gpu<<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m, n, x->dim, y->dim, alpha, beta,
                                          matrix_d, (double *) args[0].data_d,
                                          (double *) args[1].data_d, set->size);
     }
