6cfa86d55573ebc32c3c091f05c026082a751324
Toby Flynn <toby_flynn@btinternet.com>
2023-04-25 18:27:52 +0100

    Added templates for AoS blas

--- src/blas/dg_op2_gemv_kernel.cu
+++ src/blas/dg_op2_gemv_kernel.cu
@@ -5,6 +5,7 @@
 #include "dg_compiler_defs.h"
 
 #include "kernels/non_templated_aos.h"
+#include "kernels/templated_aos.h"
 
 void custom_kernel_gemv(op_set set, const bool t, const int m, const int n, const DG_FP alpha,
   const DG_FP beta, const DG_FP *matrix, op_dat x, op_dat y) {
@@ -27,15 +28,97 @@ void custom_kernel_gemv(op_set set, const bool t, const int m, const int n, cons
     if(t) {
       const int nblocks = set->size * n / nthread + 1;
       const int ncells = nthread / n + 3;
-      cuda_gemm_T_gpu<<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m, n, x->dim, y->dim, alpha, beta,
-                                           matrix_d, (double *) args[0].data_d,
-                                           (double *) args[1].data_d, set->size);
+      switch(m) {
+        // The number of nodes for each order
+        case 4:
+          templated_cuda_gemm_T_gpu<4><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(n,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 10:
+          templated_cuda_gemm_T_gpu<10><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(n,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 20:
+          templated_cuda_gemm_T_gpu<20><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(n,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        // The number of face nodes for each order
+        case 12:
+          templated_cuda_gemm_T_gpu<12><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(n,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 24:
+          templated_cuda_gemm_T_gpu<24><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(n,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 40:
+          templated_cuda_gemm_T_gpu<40><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(n,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        default:
+          cuda_gemm_T_gpu<<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m, n, x->dim, y->dim, alpha, beta,
+                                               matrix_d, (double *) args[0].data_d,
+                                               (double *) args[1].data_d, set->size);
+      }
     } else {
       const int nblocks = set->size * m / nthread + 1;
       const int ncells = nthread / m + 3;
-      cuda_gemm_gpu<<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m, n, x->dim, y->dim, alpha, beta,
-                                         matrix_d, (double *) args[0].data_d,
-                                         (double *) args[1].data_d, set->size);
+      switch(n) {
+        // The number of nodes for each order
+        case 4:
+          templated_cuda_gemm_gpu<4><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 10:
+          templated_cuda_gemm_gpu<10><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 20:
+          templated_cuda_gemm_gpu<20><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        // The number of face nodes for each order
+        case 12:
+          templated_cuda_gemm_gpu<12><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 24:
+          templated_cuda_gemm_gpu<24><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        case 40:
+          templated_cuda_gemm_gpu<40><<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m,
+                                              x->dim, y->dim, alpha, beta,
+                                              matrix_d, (double *) args[0].data_d,
+                                              (double *) args[1].data_d, set->size);
+          break;
+        default:
+          cuda_gemm_gpu<<<nblocks,nthread,(m*n + ncells * x->dim)*sizeof(double)>>>(m, n, x->dim, y->dim, alpha, beta,
+                                             matrix_d, (double *) args[0].data_d,
+                                             (double *) args[1].data_d, set->size);
+      }
     }
 
     cudaFree(matrix_d);
