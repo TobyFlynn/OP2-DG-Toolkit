4ee158d009472dcb8e385e9d85862ac76ff50eee
Toby Flynn <toby_flynn@btinternet.com>
2023-06-27 13:48:30 +0100

    Updating CPU OP2 BLAS for forcing halo exchange

--- src/blas/dg_op2_blas.cpp
+++ src/blas/dg_op2_blas.cpp
@@ -87,6 +87,75 @@ void op2_cpu_gemm(const int m, const int n, const int k,
   op_mpi_set_dirtybit(2, args);
 }
 
+void op2_cpu_gemm_halo_exchange(const int m, const int k,
+                  const DG_FP alpha, const bool trans, const DG_FP *A,
+                  const int lda, op_dat b_dat, const int ldb, const DG_FP beta,
+                  op_dat c_dat, const int ldc) {
+  op_arg args[] = {
+    op_arg_dat(b_dat, -1, OP_ID, b_dat->dim, DG_FP_STR, OP_READ),
+    op_arg_dat(c_dat, -1, OP_ID, c_dat->dim, DG_FP_STR, OP_RW)
+  };
+  op_mpi_halo_exchanges_grouped(b_dat->set, 2, args, 1, 1);
+
+  for(int round = 0; round < 2; round++) {
+    if(round == 1)
+      op_mpi_wait_all_grouped(2, args, 1, 1);
+    const int n = round == 0 ? b_dat->set->core_size : b_dat->set->size + b_dat->set->exec_size + b_dat->set->nonexec_size;
+    const int start = round == 0 ? 0 : b_dat->set->core_size;
+    const int round_size = n - start;
+    const DG_FP *B = (DG_FP *)b_dat->data + start * ldb;
+    DG_FP *C = (DG_FP *)c_dat->data + start * ldc;
+    #ifdef OP2_DG_USE_LIBXSMM
+    const int flags = trans ? LIBXSMM_GEMM_FLAG_TRANS_A : LIBXSMM_GEMM_FLAG_NONE;
+    const int prefetch = LIBXSMM_PREFETCH_NONE;
+    const int batch_size = 32;
+    const char transA = trans ? 't' : 'n';
+    const char noTrans = 'n';
+
+    libxsmm_dmmfunction xmm = NULL;
+    if(!trans && alpha == 1.0 && (beta == 1.0 || beta == 0.0))
+      xmm = libxsmm_dmmdispatch(m, batch_size, k, &lda, &ldb, &ldc, &alpha, &beta, &flags, &prefetch);
+
+    if(xmm) {
+      #pragma omp parallel for
+      for(int i = 0; i < round_size / batch_size; i++) {
+        const DG_FP *_B = B + i * batch_size * ldb;
+        DG_FP *_C = C + i * batch_size * ldc;
+        // xmm(A, _B, _C, A, _B + batch_size * DG_NUM_FACES * DG_NPF, _C + batch_size * DG_NP);
+        xmm(A, _B, _C);
+      }
+    } else {
+      #pragma omp parallel for
+      for(int i = 0; i < round_size / batch_size; i++) {
+        const DG_FP *_B = B + i * batch_size * ldb;
+        DG_FP *_C = C + i * batch_size * ldc;
+        libxsmm_dgemm(&transA, &noTrans, &m, &batch_size, &k, &alpha, A, &lda, _B, &ldb, &beta, _C, &ldc);
+      }
+    }
+
+    if((round_size / batch_size) * batch_size != round_size) {
+      const DG_FP *_B = B + ((round_size / batch_size) * batch_size) * ldb;
+      DG_FP *_C = C + ((round_size / batch_size) * batch_size) * ldc;
+      const int final_n = round_size - ((round_size / batch_size) * batch_size);
+      libxsmm_dgemm(&transA, &noTrans, &m, &final_n, &k, &alpha, A, &lda, _B, &ldb, &beta, _C, &ldc);
+    }
+    #else
+    const int batch_size = round_size / 32;
+    const int num_batches = round_size / batch_size;
+    const CBLAS_TRANSPOSE transA = trans ? CblasTrans : CblasNoTrans;
+    #pragma omp parallel for
+    for(int i = 0; i < num_batches; i++) {
+      cblas_dgemm(CblasColMajor, transA, CblasNoTrans, m, batch_size, k, alpha, A, lda, B + i * batch_size * ldb, ldb, beta, C + i * batch_size * ldc, ldc);
+    }
+    if(round_size != batch_size * num_batches) {
+      const int left_over_n = round_size - batch_size * num_batches;
+      cblas_dgemm(CblasColMajor, transA, CblasNoTrans, m, left_over_n, k, alpha, A, lda, B + num_batches * batch_size * ldb, ldb, beta, C + num_batches * batch_size * ldc, ldc);
+    }
+    #endif
+  }
+  op_mpi_set_dirtybit(2, args);
+}
+
 #else
 void custom_kernel_gemv(op_set set, const bool t, const int m, const int n, const DG_FP alpha,
   const DG_FP beta, const DG_FP *matrix, op_dat arg4, op_dat arg5);
@@ -390,21 +459,21 @@ void op2_gemv_interp(DGMesh *mesh, const int from_N, const int to_N, op_dat x, o
 void op2_gemv_np_np_halo_exchange(DGMesh *mesh, bool transpose, const DG_FP alpha,
                     const DG_FP *matrix, op_dat x, const DG_FP beta,
                     op_dat y) {
-  if(transpose) {
-    throw std::runtime_error("gemv_halo_exchange not fully supported yet\n");
-  } else {
-    #if defined(USE_OP2_KERNELS) || (defined(OP2_DG_CUDA) && DG_DIM == 2)
-    throw std::runtime_error("gemv_halo_exchange not fully supported yet\n");
-    #elif defined(OP2_DG_CUDA)
-    const int order = ((DGMesh3D *)mesh)->order_int;
-    const int m = DG_CONSTANTS_TK[(order - 1) * DG_NUM_CONSTANTS];
-    const int k = m;
-    const DG_FP *A = matrix + (order - 1) * DG_NP * DG_NP;
-    custom_kernel_gemv_halo_exchange(mesh->cells, false, m, k, alpha, beta, A, x, y);
-    #else
-    throw std::runtime_error("gemv_halo_exchange not fully supported yet\n");
-    #endif
-  }
+  #if defined(USE_OP2_KERNELS) || (defined(OP2_DG_CUDA) && DG_DIM == 2)
+  throw std::runtime_error("gemv_halo_exchange not fully supported yet\n");
+  #elif defined(OP2_DG_CUDA)
+  const int order = ((DGMesh3D *)mesh)->order_int;
+  const int m = DG_CONSTANTS_TK[(order - 1) * DG_NUM_CONSTANTS];
+  const int k = m;
+  const DG_FP *A = matrix + (order - 1) * DG_NP * DG_NP;
+  custom_kernel_gemv_halo_exchange(mesh->cells, transpose, m, k, alpha, beta, A, x, y);
+  #else
+  const int order = *((int *)mesh->order->data);
+  const int m = DG_CONSTANTS_TK[(order - 1) * DG_NUM_CONSTANTS];
+  const int k = m;
+  const DG_FP *A = matrix + (order - 1) * DG_NP * DG_NP;
+  op2_cpu_gemm_halo_exchange(m, k, alpha, transpose, A, m, x, DG_NP, beta, y, DG_NP);
+  #endif
 }
 
 void op2_gemv_halo_exchange(DGMesh *mesh, bool transpose, const DG_FP alpha,
