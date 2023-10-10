a0b151de45a6e7f8fc2fa33f1b3f30d262fbed55
Toby Flynn <toby_flynn@btinternet.com>
2023-10-10 16:48:18 +0100

    Reduced time to change orders

--- src/dg_mesh/dg_mesh_3d.cpp
+++ src/dg_mesh/dg_mesh_3d.cpp
@@ -97,6 +97,12 @@ DGMesh3D::~DGMesh3D() {
 }
 
 void DGMesh3D::init() {
+  op_par_loop(init_nodes_3d, "init_nodes_3d", cells,
+              op_arg_dat(node_coords, -4, cell2nodes, 3, DG_FP_STR, OP_READ),
+              op_arg_dat(nodeX, -1, OP_ID, 4, DG_FP_STR, OP_WRITE),
+              op_arg_dat(nodeY, -1, OP_ID, 4, DG_FP_STR, OP_WRITE),
+              op_arg_dat(nodeZ, -1, OP_ID, 4, DG_FP_STR, OP_WRITE));
+
   op_par_loop(init_grid_3d, "init_grid_3d", cells,
               op_arg_gbl(&order_int, 1, "int", OP_READ),
               op_arg_dat(nodeX, -1, OP_ID, 4, DG_FP_STR, OP_READ),
@@ -111,41 +117,6 @@ void DGMesh3D::init() {
               op_arg_dat(y, -1, OP_ID, DG_NP, DG_FP_STR, OP_READ),
               op_arg_dat(z, -1, OP_ID, DG_NP, DG_FP_STR, OP_READ),
               op_arg_dat(geof, -1, OP_ID, 10, DG_FP_STR, OP_WRITE));
-
-  op_par_loop(init_nodes_3d, "init_nodes_3d", cells,
-              op_arg_dat(node_coords, -4, cell2nodes, 3, DG_FP_STR, OP_READ),
-              op_arg_dat(nodeX, -1, OP_ID, 4, DG_FP_STR, OP_WRITE),
-              op_arg_dat(nodeY, -1, OP_ID, 4, DG_FP_STR, OP_WRITE),
-              op_arg_dat(nodeZ, -1, OP_ID, 4, DG_FP_STR, OP_WRITE));
-
-  calc_mesh_constants();
-}
-
-void DGMesh3D::calc_mesh_constants() {
-  op_par_loop(init_grid_3d, "init_grid_3d", cells,
-              op_arg_gbl(&order_int, 1, "int", OP_READ),
-              op_arg_dat(nodeX, -1, OP_ID, 4, DG_FP_STR, OP_READ),
-              op_arg_dat(nodeY, -1, OP_ID, 4, DG_FP_STR, OP_READ),
-              op_arg_dat(nodeZ, -1, OP_ID, 4, DG_FP_STR, OP_READ),
-              op_arg_dat(x, -1, OP_ID, DG_NP, DG_FP_STR, OP_WRITE),
-              op_arg_dat(y, -1, OP_ID, DG_NP, DG_FP_STR, OP_WRITE),
-              op_arg_dat(z, -1, OP_ID, DG_NP, DG_FP_STR, OP_WRITE));
-
-  int num = 0;
-  op_par_loop(face_check_3d, "face_check_3d", faces,
-              op_arg_gbl(&order_int, 1, "int", OP_READ),
-              op_arg_dat(faceNum, -1, OP_ID, 2, "int", OP_READ),
-              op_arg_dat(periodicFace, -1, OP_ID, 1, "int", OP_READ),
-              op_arg_dat(x, -2, face2cells, DG_NP, DG_FP_STR, OP_READ),
-              op_arg_dat(y, -2, face2cells, DG_NP, DG_FP_STR, OP_READ),
-              op_arg_dat(z, -2, face2cells, DG_NP, DG_FP_STR, OP_READ),
-              op_arg_dat(fmaskL, -1, OP_ID, DG_NPF, "int", OP_WRITE),
-              op_arg_dat(fmaskR, -1, OP_ID, DG_NPF, "int", OP_WRITE),
-              op_arg_gbl(&num, 1, "int", OP_INC));
-  if(num > 0) {
-    std::cout << "Number of non matching points on faces: " << num << std::endl;
-    exit(-1);
-  }
 
   op_par_loop(init_faces_3d, "init_faces_3d", faces,
               op_arg_dat(faceNum, -1, OP_ID, 2, "int", OP_READ),
@@ -208,6 +179,35 @@ void DGMesh3D::calc_mesh_constants() {
                 op_arg_dat(ny_c, 0, bface2cells, 4, DG_FP_STR, OP_WRITE),
                 op_arg_dat(nz_c, 0, bface2cells, 4, DG_FP_STR, OP_WRITE),
                 op_arg_dat(sJ_c, 0, bface2cells, 4, DG_FP_STR, OP_WRITE));
+  }
+
+  calc_mesh_constants();
+}
+
+void DGMesh3D::calc_mesh_constants() {
+  op_par_loop(init_grid_3d, "init_grid_3d", cells,
+              op_arg_gbl(&order_int, 1, "int", OP_READ),
+              op_arg_dat(nodeX, -1, OP_ID, 4, DG_FP_STR, OP_READ),
+              op_arg_dat(nodeY, -1, OP_ID, 4, DG_FP_STR, OP_READ),
+              op_arg_dat(nodeZ, -1, OP_ID, 4, DG_FP_STR, OP_READ),
+              op_arg_dat(x, -1, OP_ID, DG_NP, DG_FP_STR, OP_WRITE),
+              op_arg_dat(y, -1, OP_ID, DG_NP, DG_FP_STR, OP_WRITE),
+              op_arg_dat(z, -1, OP_ID, DG_NP, DG_FP_STR, OP_WRITE));
+
+  int num = 0;
+  op_par_loop(face_check_3d, "face_check_3d", faces,
+              op_arg_gbl(&order_int, 1, "int", OP_READ),
+              op_arg_dat(faceNum, -1, OP_ID, 2, "int", OP_READ),
+              op_arg_dat(periodicFace, -1, OP_ID, 1, "int", OP_READ),
+              op_arg_dat(x, -2, face2cells, DG_NP, DG_FP_STR, OP_READ),
+              op_arg_dat(y, -2, face2cells, DG_NP, DG_FP_STR, OP_READ),
+              op_arg_dat(z, -2, face2cells, DG_NP, DG_FP_STR, OP_READ),
+              op_arg_dat(fmaskL, -1, OP_ID, DG_NPF, "int", OP_WRITE),
+              op_arg_dat(fmaskR, -1, OP_ID, DG_NPF, "int", OP_WRITE),
+              op_arg_gbl(&num, 1, "int", OP_INC));
+  if(num > 0) {
+    std::cout << "Number of non matching points on faces: " << num << std::endl;
+    exit(-1);
   }
 
   #ifdef USE_CUSTOM_MAPS
