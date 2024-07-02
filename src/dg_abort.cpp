39a4fbc2a28dd99c1a278fd5e6e90b05de7057a9
Toby Flynn <toby_flynn@btinternet.com>
2024-07-02 11:28:41 +0100

    Bug fix for dg_abort

--- src/dg_abort.cpp
+++ src/dg_abort.cpp
@@ -9,7 +9,7 @@
 
 void dg_abort(const std::string &msg) {
 #ifdef DG_MPI
-  fprintf(stderr, "%s\n", msg);
+  fprintf(stderr, "%s\n", msg.c_str());
   MPI_Abort(MPI_COMM_WORLD, 0);
 #else
   throw std::runtime_error(msg);
